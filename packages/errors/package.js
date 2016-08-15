63a97c2bc2497c0a926dd65865946ecd791142f5
龚传栋 <413883127@qq.com>
2016-08-15 09:43:46 +0800

    test

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -1,24 +1,24 @@
 Package.describe({
-  name: 'gcdong:errors',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'gcdong:errors',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: '用于本app错误提示',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/gcdong/myapp',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.0.1');
-  api.use('ecmascript');
-  api.mainModule('errors.js');
+    api.versionsFrom('1.4.0.1');
+    api.use('ecmascript');
+    api.mainModule('errors.js');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('gcdong:errors');
-  api.mainModule('errors-tests.js');
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('gcdong:errors');
+    api.mainModule('errors-tests.js');
 });
